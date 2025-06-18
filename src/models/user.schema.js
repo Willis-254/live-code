23da292aed3a54b07c2485e40300482afae0159e
William Njoroge <126962572+Willis-254@users.noreply.github.com>
2025-06-18 10:01:10 +0000

    REctification

--- src/models/user.schema.js
+++ src/models/user.schema.js
@@ -1,12 +1,35 @@
 import mongoose from "mongoose"
+import AuthRoles from "../utils/authRoles.js"
 
 const userSchema=new mongoose.Schema(
     {
         name: {
+        type: String,
+        required: ["true","Please provide a product name"],
+        trim: true,
+        maxLength: [
+            120,
+            "Collection name should not be more than 120 chars"
+            ]
+        },
+        email: {
             type: String,
-            required: ["true", "Name is required"],
-            maxLe
-        }
-    },{timestamps:true})
+            required: ["true","Email is required"]
+        },
+        password: {
+            type: String,
+            required: ["true","Pasword is required"],
+            minLength: [8,"password must atleast 8 characters"],
+            select: false
+        },
+        role: {
+            type: String,
+            enum: Object.values(AuthRoles),
+            default: AuthRoles.USER
+        },
+        forgotPasswordToken: String,
+        forgotPasswordExpiry: Date,
+    },
+        {timestamps:true})
 
 export default mongoose.model("User",userSchema)

d9dc9cbf97697b03d9287d7d1aff589105d705d8
William Njoroge <126962572+Willis-254@users.noreply.github.com>
2025-06-18 11:10:10 +0000

    Hooks and Methods in Mongoose

--- src/models/user.schema.js
+++ src/models/user.schema.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose"
 import AuthRoles from "../utils/authRoles.js"
+import bcrypt from "bcryptjs"
 
 const userSchema=new mongoose.Schema(
     {
@@ -31,5 +32,17 @@ const userSchema=new mongoose.Schema(
         forgotPasswordExpiry: Date,
     },
         {timestamps:true})
+
+userSchema.pre("save",async function(next){
+    if(!this.isModified("password")) return next()
+    this.password=await bcrypt.hash(this.password, 10)
+    next()
+})
+
+userSchema.methods={
+    comparePassword: async function (enteredPassword) {
+        return await bcrypt.compare(enteredPassword, this.password)
+    }
+}
 
 export default mongoose.model("User",userSchema)

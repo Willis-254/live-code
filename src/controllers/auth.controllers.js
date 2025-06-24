df562e14fdece73b98a3f8371c674f2f75dd2836
William Njoroge <126962572+Willis-254@users.noreply.github.com>
2025-06-24 17:49:36 +0000

    User Logged In and Out

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -38,3 +38,44 @@ export const signUp=asyncHandler(async(req,res)=>{
     }
     )
 })
+
+export const login=asyncHandler(async(req,res)=>{
+    const {name, email, body}=req.body
+
+    if(!name || !email){
+        throw new CustomError("Please add all fields",400)
+    }
+
+    const user=User.findOne({email}).select("+password")
+
+    if(!user){
+        throw new CustomError("Invalid Credentials",400)
+    }
+
+    const isPasswordMatched= await user.comparepassword(password)
+
+    if(isPasswordMatched){
+        const token=user.getJWTtoken
+        user.password=undefined
+        res.cookie("token",token,cookieOPtion)
+        return res.status(200).json({
+            success: true,
+            token,
+            user
+        })
+    }
+
+    throw new CustomError("Password is incorrect",400)
+})
+
+export const logout=asyncHandler(async(req,res)=>{
+    res.cookie("token",null,{
+        expires: new Date(Date.now()),
+        httpOnly: true
+    })
+
+    res.status(200).json({
+        success: true,
+        message: "Logged Out"
+    })
+})

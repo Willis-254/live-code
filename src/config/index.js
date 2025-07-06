74c44a8cacd5810e2c8687cd03375494ffd62893
William Njoroge <126962572+Willis-254@users.noreply.github.com>
2025-07-06 11:05:03 +0000

    Uploaded image to s3

--- src/config/index.js
+++ src/config/index.js
@@ -6,7 +6,11 @@ const config={
     PORT:process.env.PORT|| 5000,
     MONGODB_URL: process.env.MONGODB_URL || "mongodb://localhost:27017/ecomm",
     JWT_SECRET: process.env.JWT_SECRET || "puppy",
-    JWT_EXPIRY: process.env.JWT_EXPIRY || "1d"
+    JWT_EXPIRY: process.env.JWT_EXPIRY || "1d",
+    S3_ACCESS_KEY: process.env.S3_ACCESS_KEY,
+    S3_SECRET_ACCESS_KEY: process.env.S3_SECRET_ACCESS_KEY,
+    S3_BUCKET_NAME: process.env.S3_BUCKET_NAME,
+    S3_REGION: process.env.S3_REGION
 }
 
 export default config

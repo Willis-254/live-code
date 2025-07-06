5319cb024024c65dcd03ba223f12e8d54041ff1d
William Njoroge <126962572+Willis-254@users.noreply.github.com>
2025-07-06 08:41:19 +0000

    Collection controller done

---
+++ src/controllers/colllection.controllers.js
@@ -0,0 +1,65 @@
+import Collection from "../models/collections.schema.js"
+import asyncHandler from "../service/asyncHandler.js"
+import CustomError from "../utils/customError"
+
+
+export const createCollection=asyncHandler(async(req,res)=>{
+    const {name}=req.body
+
+    if(!name){
+        throw new CustomError("Collection name is required",400)
+    }
+
+    const collection=await Collection.create({
+        name
+    })
+
+    res.status(200).json({
+        success: true,
+        message: "Collection waas created successfully",
+        collection
+    })
+})
+
+export const updateCollection=asyncHandler(async(req,res)=>{
+    const {name}=req.body
+    const {id: collectionID}=req.params
+
+    if(!name){
+        throw new CustomError("Collection name is required",400)
+    }
+
+    let updatedCollection=await Collection.findByIdAndUpdate(collectionID, {
+        name: name
+    },{
+        new: true,
+        runValidators: true
+    })
+
+    res.status(200).json({
+        success: true,
+        message: "Collection waas created successfully",
+        collection
+    })
+
+    if(!updatedCollection){
+        throw new CustomError("Collection not found",400)
+    }
+})
+
+export const deleteCollection=asyncHandler(async(req,res)=>{
+    const {id: collectionID}=req.params
+
+    const collectionToDelete=await Collection.findById(collectionID)
+
+    if(!collectionToDelete){
+        throw new CustomError("Collection to be deleted not found",400)
+    }
+
+    collectionToDelete.remove()
+
+    res.status(200).json({
+        success: true,
+        message: "Collection deleted successfully",
+    })
+})

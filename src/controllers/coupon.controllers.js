0202daf2ecb32f6039d3f23f91e2c80032f966c9
William Njoroge <126962572+Willis-254@users.noreply.github.com>
2025-07-28 09:12:56 +0000

    coupon controller done

--- src/controllers/coupon.controllers.js
+++ src/controllers/coupon.controllers.js
@@ -21,3 +21,79 @@ import CustomError from "../utils/customError"
 
 
  *********************************************************/
+
+
+export const createCoupon=asyncHandler(async (req, res)=>{
+    const {code, discount}=req.body
+
+    if (!code || !discount){
+        throw new CustomError("Code and Discount are required",400)
+    }
+
+    const coupon=await Coupon.create({
+        code, 
+        discount
+    })
+
+    res.status(200).json({
+        succes: true,
+        message: "Coupon created successfully"
+    })
+})
+
+export const updateCoupon=asyncHandler(async (req,res)=>{
+    const {id: couponId}=req.body
+    const {action}=req.body
+
+    const coupon=Coupon.findByIdAndUpdate(
+        couponId,
+        {
+            active: action
+        },
+        {
+            new: true,
+            runValidators: true
+        }
+    )
+
+    if(!coupon){
+        throw new CustomError("Coupon not found",404)
+    }
+
+    res.status(200).json({
+        success: true,
+        message: "Coupon updated",
+        coupon
+    })
+})
+
+export const deleteCoupon = asyncHandler(async(req, res) => {
+    const {id: couponId} = req.params
+
+    const coupon = await Coupon.findByIdAndDelete(couponId)
+
+    if (!coupon) {
+        throw new CustomError("Coupon not found", 404)
+    }
+
+    res.status(200).json({
+        success: true,
+        message: "Coupon deleted",
+        
+    })
+})
+
+export const getAllCoupons = asyncHandler( async (req, res) => {
+    const allCoupons = await Coupon.find();
+
+    if (!allCoupons) {
+        throw new CustomError("No Coupons found", 400)
+        
+    }
+
+    res.status(200).json({
+        success: true,
+        allCoupons
+    })
+    
+})
